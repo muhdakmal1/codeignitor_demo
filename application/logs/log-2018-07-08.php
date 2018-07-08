66d0b5b1554c0070a57ca1176e33c965100de9ee
muhdakmal1 <akmalater99@gmail.com>
2018-07-09 00:12:15 +0800

    Adding script for phpword
    done it

---
+++ application/logs/log-2018-07-08.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-07-08 16:44:18 --> Severity: 8192 --> __autoload() is deprecated, use spl_autoload_register() instead C:\xampp\htdocs\codeigniter_demo\application\config\config.php 356
+DEBUG - 2018-07-08 16:47:30 --> No URI present. Default controller set.
+DEBUG - 2018-07-08 16:47:36 --> No URI present. Default controller set.
+DEBUG - 2018-07-08 16:47:39 --> No URI present. Default controller set.
+ERROR - 2018-07-08 17:21:35 --> Severity: error --> Exception: Class 'PhpOffice\PhpWord\Element\String' not found C:\xampp\htdocs\codeigniter_demo\application\third_party\PhpWord\Element\Text.php 139
+ERROR - 2018-07-08 17:23:54 --> Severity: error --> Exception: Class 'PhpOffice\PhpWord\Style\String' not found C:\xampp\htdocs\codeigniter_demo\application\third_party\PhpWord\Style\Paragraph.php 179
+ERROR - 2018-07-08 17:40:07 --> Severity: error --> Exception: Class 'PhpOffice\PhpWord\Style\String' not found C:\xampp\htdocs\codeigniter_demo\application\third_party\PhpWord\Style\AbstractStyle.php 164
+ERROR - 2018-07-08 17:40:52 --> Severity: error --> Exception: Class 'PhpOffice\PhpWord\Writer\Word2007\Element\String' not found C:\xampp\htdocs\codeigniter_demo\application\third_party\PhpWord\Writer\Word2007\Element\AbstractElement.php 170
+ERROR - 2018-07-08 17:41:03 --> Severity: Warning --> Illegal string offset 'w:compatSetting' C:\xampp\htdocs\codeigniter_demo\application\third_party\PhpWord\Writer\Word2007\Part\Settings.php 173
+ERROR - 2018-07-08 17:41:03 --> Severity: Notice --> Array to string conversion C:\xampp\htdocs\codeigniter_demo\application\third_party\PhpWord\Writer\Word2007\Part\Settings.php 173
+ERROR - 2018-07-08 17:41:03 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\codeigniter_demo\application\third_party\PhpWord\Writer\Word2007\Part\Settings.php 80
